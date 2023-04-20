0c19aeba5b2b0263d7e1d0dae25a5a87083a507b
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2023-04-21 01:41:47 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,31 +1,31 @@
-import os
+import os import environ
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = environ['BOT_TOKEN']
 
-    SESSION_NAME = ""
+    SESSION_NAME = environ['SESSION_NAME']
 
-    API_ID = ""
+    API_ID = environ['API_ID']
 
-    API_HASH = ""
+    API_HASH = environ['API_HASH']
 
-    CLIENT_ID = ""
+    CLIENT_ID = environ['CLIENT_ID']
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = CLIENT_SECRET']
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = list({int(x) for x in environ.get("AUTH_USERS", "").split()})
 
-    VIDEO_DESCRIPTION = ""
+    VIDEO_DESCRIPTION = environ['VIDEO_DESCRIPTION']
 
-    VIDEO_CATEGORY = ""
+    VIDEO_CATEGORY = environ['VIDEO_CATEGORY']
 
-    VIDEO_TITLE_PREFIX = ""
+    VIDEO_TITLE_PREFIX = environ['VIDEO_TITLE_PREFIX']
 
-    VIDEO_TITLE_SUFFIX = ""
+    VIDEO_TITLE_SUFFIX = environ['VIDEO_TITLE_SUFFIX']
     
     DEBUG = bool()
 
-    UPLOAD_MODE = "unlisted"
+    UPLOAD_MODE = environ['UPLOAD_MODE']
     
     CRED_FILE = "auth_token.txt"
